cc6a2a1f14f4a1eae5c931990dba99173c30f139
Ziyi An <ziyia2@uw.edu>
2023-12-01 23:33:54 -0500

    finish environment settings

--- users/routes.js
+++ users/routes.js
@@ -1,22 +1,61 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+// let currentUser = null;
 function UserRoutes(app) {
-  const createUser = async (req, res) => { };
-  const deleteUser = async (req, res) => { };
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+
+   };
+  const deleteUser = async (req, res) => { 
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
   const findAllUsers = async (req, res) => { 
     const users = await dao.findAllUsers();
     res.json(users);
    };
-  const findUserById = async (req, res) => { };
-  const updateUser = async (req, res) => { };
-  const signup = async (req, res) => { };
-  const signin = async (req, res) => { 
-    const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+
+   };
+  const updateUser = async (req, res) => { 
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    const currentUser = await dao.findUserById(userId);
+    req.session['currentUser'] = currentUser;
+    res.json(status);
+  };
+
+  const signup = async (req, res) => { 
+    const user = await dao.findUserByUsername(
+      req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already taken" });
+    }
+    const currentUser = await dao.createUser(req.body);
+    req.session['currentUser'] = currentUser;
     res.json(currentUser);
   };
-  const signout = (req, res) => { };
-  const account = async (req, res) => { };
+  const signin = async (req, res) => { 
+    const { username, password } = req.body;
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      // currentUser = user;
+      req.session['currentUser'] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.sendStatus(403);
+    }
+  };
+  const signout = (req, res) => { 
+    req.session.destroy();
+    res.json(200);
+  };
+  const accountUser = async (req, res) => { 
+    res.json(req.session['currentUser']);
+  };
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
@@ -25,6 +64,6 @@ function UserRoutes(app) {
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
-  app.post("/api/users/account", account);
+  app.post("/api/users/accountUser", accountUser);
 }
 export default UserRoutes;
